83d5f3dd83b7d6dc027a22ba8e58562a1ab94a18
Darren Fung <darren@earnwithdrop.com>
2017-03-16 10:48:57 -0400

    Adds limit to account

--- lib/plaid/models/account.rb
+++ lib/plaid/models/account.rb
@@ -1,17 +1,18 @@
 module Plaid
   class Account
-    attr_accessor :available_balance, :current_balance, :institution_type, :meta, :transactions, :numbers, :name, :id, :type, :subtype
+    attr_accessor :available_balance, :current_balance, :limit, :institution_type, :meta, :transactions, :numbers, :name, :id, :type, :subtype
 
     def initialize(hash)
-      @id   = hash['_id']
-      @name = hash['meta']['name'] if hash['meta']
-      @type = hash['type']
-      @meta = hash['meta']
-      @institution_type  = hash['institution_type']
+      @id               = hash['_id']
+      @name             = hash['meta']['name'] if hash['meta']
+      @type             = hash['type']
+      @meta             = hash['meta']
+      @institution_type = hash['institution_type']
 
       if hash['balance']
         @available_balance = hash['balance']['available']
         @current_balance   = hash['balance']['current']
+        @limit             = hash['balance']['limit']
       end
 
       # Depository account only, "checkings" or "savings"
